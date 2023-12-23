5e4d171771ef547a870a987a1f47dd69fd0ce856
lahiruthennakoon <lahiruthennakoon@gmail.com>
2023-12-24 02:57:00 +0530

    add basic files

---
+++ playwright.config.js
@@ -0,0 +1,28 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    browserName:'chromium',
+    actionTimeout:30*1000
+    
+  },
+
+});
+
